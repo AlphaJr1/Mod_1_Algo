3587803bde32b392d461725cdbe67a5bf2972fc8
AlphaJr1 <113828544+AlphaJr1@users.noreply.github.com>
2022-09-20 21:21:52 +0700

    Add files via upload

---
+++ Latihan 1-Praktikum.py
@@ -0,0 +1,13 @@
+#Adrian Alfajri - 064002200009
+a = input("Masukkan namamu= ")
+b = input("Masukkan asal tinggal= ")
+c = input("Masukkan sekolah sebelumnya= ")
+
+print(" ")
+print("Nama kamu siapa? Halo nama saya "+ a)
+print("Darimana asal kamu? Saya berasal dari "+ b)
+print("Sebelumnya bersekolah dimana? Saya bersekolah di  "+ c)
+print(" ")
+print("Hallo selamat siang "+ a)
+print("Wahh dari "+ b)
+print("Pasti seru banget tuh sekolah di "+ c)
---
+++ Latihan 2-Praktikum.py
@@ -0,0 +1,12 @@
+#Adrian Alfajri - 064002200009
+print("Hitung Luas Ruangan")
+P = int(input("Masukkan Panjang Ruangan: "))
+L = int(input("Masukkan Lebar Ruangan: "))
+S = input("Masukkan Satuan (Meter/Inci): ")
+x = P*L
+
+p2 = str(P)
+l2 = str(L)
+x2 = str(x)
+print("Luas ruangan dengan panjang "+ p2 + " dan lebar "+ l2 +" adalah "+ x2 + " " + S)
+
